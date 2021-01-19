3fdf75c5554bfb0dba89973fc0873608ba8fe9b6
valeria-ab <offtotheraces@inbox.ru>
2021-01-19 18:23:12 +0300

    added second block

---
+++ src/main/Main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import style from './Main.module.css';
+
+
+function Main() {
+    return (
+        <div className={style.mainBlock}>
+            <div className={style.container}>
+                <div className={style.text}>
+                    <span>Hi There</span>
+                    <h1>I am Valeria Abutkina</h1>
+                    <p>A frontend developer.</p>
+                </div>
+                <div className={style.photo}></div>
+
+            </div>
+        </div>
+    );
+}
+
+export default Main;
